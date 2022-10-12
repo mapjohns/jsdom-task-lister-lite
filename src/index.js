003530045d1170510321c2cf7b597f4070302c88
Matthew <mattj20@gmail.com>
2022-10-11 17:45:51 -0700

    commit tree 875209c93a08ec39e9e5c55c95e92b0bffa0de02

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log("DOM loaded.")
 });
+
+document.querySelector('form input[type="submit"]').addEventListener('click', addTaskToList)
+document.getElementById('new-task-description').addEventListener('input', updateValue)
+document.getElementById('tasks')
+
+let task
+
+function updateValue(e) {
+  task = e.target.value
+}
+
+function addTaskToList(e) {
+  e.preventDefault();
+  let addTask = document.createElement('li')
+  let button = document.createElement("button")
+  button.innerHTML = "X"
+  addTask.innerHTML = `${task} `
+  addTask.append(button)
+  document.querySelector('ul#tasks').append(addTask)
+  clearBox()
+}
+
+function clearBox() {
+  document.getElementById('new-task-description').value = ""
+  task = ""
+}
